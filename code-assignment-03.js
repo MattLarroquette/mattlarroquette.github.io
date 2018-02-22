c501b04100c0a0d9d34e00b8b546df67ce507760
MattLarroquette <mlarroquette4@gmail.com>
2018-02-22 10:46:27 -0600

    Add Book 6 entry and code 3

---
+++ code-assignment-03.js
@@ -0,0 +1,9 @@
+let numProjects = "5"
+let projectTitle = "US Cities Growth"
+let hasScreenshot = true
+let tags = ['ch6', 'US', 'Cities', 'Growth']
+
+console.log('Number of Portfolio Entires: ' + numProjects)
+console.log('Most Recent Map Title: ' + projectTitle)
+console.log('Has Screenshot? ' + hasScreenshot)
+console.log('Map Tags: ' + tags)
